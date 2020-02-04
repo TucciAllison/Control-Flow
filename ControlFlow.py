2c150f8dd9572dc4e83f5abd8458b9632e64ca6e
Allison Tucci <02allison.tucci@gulllakecs.org>
2020-02-04 08:16:22 -0500

    Add Default Parameter Values

--- ControlFlow.py
+++ ControlFlow.py
@@ -1,15 +1,39 @@
-# Date: 2.3.2020
-# Greeting Functions
 
-name = input("\nWhat is your name? ")
+# Programmer: Allison Tucci
+# Date: 2.3.2020
+
+#Declare Global Variables Here
+# name = input("\nWhat is your name? ")
+x = 15
 
 #Create Functions Here
 
+# Greeting Function
 def greeting():
-    print ("Hi there " + name + "!")
-    print ("Very nice to meet you " + name)
+    print("Hi there " + name + "!")
+    print("Very nice to meet you " + name)
+    print(x)
+
+#Functions and Local Variable x
+def printSomething():
+    x = 3
+    print(x)
+
+#Functions and Parameters
+def printNumber(age): #Function name = printNumber with a PARAMETER of age
+    print(age)
+
+#Default Parameter Values
+def printTwoNumbers(x,y= 71):
+    print("First Parameter(number): " + str(x))
+    print("Second Parameter(number): " + str(y))
+
 
 #Call Functions Here
-greeting()
-
-
+# greeting()
+# printSomething()
+# print(x)
+#printNumber(28)
+#printNumber(38)
+printTwoNumbers(23,78)
+printTwoNumbers(45)
