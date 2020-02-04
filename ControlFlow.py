d519f31e728bace99e395f381fd7468a27d136a8
Allison Tucci <02allison.tucci@gulllakecs.org>
2020-02-04 08:36:42 -0500

    Add Print Multiple Times Function

--- ControlFlow.py
+++ ControlFlow.py
@@ -32,6 +32,10 @@ def printTwoNumbers(x,y= 71):
 def printSum(x,y):
     print(x + y)
 
+#Print Multiple Times
+def printMultipleTimes(string, times):
+    for i in range(times):
+        print(string)
 
 #Call Functions Here
 # greeting()
@@ -41,4 +45,5 @@ def printSum(x,y):
 #printNumber(38)
 #printTwoNumbers(23,78)
 #printTwoNumbers(45)
-printSum(1,17)
+#printSum(36,29)
+printMultipleTimes("I love computer science", 13)
